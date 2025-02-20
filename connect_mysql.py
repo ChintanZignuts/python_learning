59bc937afedd51a4be096e5e4690aad9f8cc264d
ChitanZignuts <chintanr@zignuts.com>
2025-02-20 11:25:17 +0530

    add functionality to update and select ratings in the movie rating database

--- connect_mysql.py
+++ connect_mysql.py
@@ -113,6 +113,57 @@ reviewers_records = [
     ("Marlon", "Crafford"),
 ]
 
+insert_ratings_query="""
+INSERT INTO ratings
+(rating,movie_id,reviewer_id)
+values(%s, %s, %s)
+"""
+
+ratings_values=[ (6.4, 17, 5), (5.6, 19, 1), (6.3, 22, 14), (5.1, 21, 17),
+    (5.0, 5, 5), (6.5, 21, 5), (8.5, 30, 13), (9.7, 6, 4),
+    (8.5, 24, 12), (9.9, 14, 9), (8.7, 26, 14), (9.9, 6, 10),
+    (5.1, 30, 6), (5.4, 18, 16), (6.2, 6, 20), (7.3, 21, 19),
+    (8.1, 17, 18), (5.0, 7, 2), (9.8, 23, 3), (8.0, 22, 9),
+    (8.5, 11, 13), (5.0, 5, 11), (5.7, 8, 2), (7.6, 25, 19),
+    (5.2, 18, 15), (9.7, 13, 3), (5.8, 18, 8), (5.8, 30, 15),
+    (8.4, 21, 18), (6.2, 23, 16), (7.0, 10, 18), (9.5, 30, 20),
+    (8.9, 3, 19), (6.4, 12, 2), (7.8, 12, 22), (9.9, 15, 13),
+    (7.5, 20, 17), (9.0, 25, 6), (8.5, 23, 2), (5.3, 30, 17),
+    (6.4, 5, 10), (8.1, 5, 21), (5.7, 22, 1), (6.3, 28, 4),
+    (9.8, 13, 1)]
+
+select_movies_query = "SELECT title, release_year FROM movies LIMIT 5"
+
+select_movies_with_join_query="""
+SELECT title, AVG(rating) AS average_rating
+FROM movies
+INNER JOIN ratings ON movies.id=ratings.movie_id
+GROUP BY movie_id
+ORDER BY average_rating DESC
+"""
+
+update_query = """
+UPDATE
+    reviewers
+SET
+    last_name = "Cooper"
+WHERE
+    first_name = "Amy"
+"""
+
+movie_id = input("Enter movie id: ")
+reviewer_id = input("Enter reviewer id: ")
+new_rating = input("Enter new rating: ")
+
+update_rating_query = """
+UPDATE ratings
+SET rating = %s
+WHERE movie_id = %s AND reviewer_id = %s;
+"""
+
+select_rating_query = """
+SELECT * FROM ratings WHERE movie_id = %s AND reviewer_id = %s
+"""
 try:
     with connect(
         host="localhost",
@@ -121,8 +172,12 @@ try:
         database="movie_rating"
     ) as connection:
         with connection.cursor() as cursor:
-            cursor.execute(select_movie_query)
-            result=cursor.fetchall()
+            cursor.execute(update_rating_query, (new_rating, movie_id, reviewer_id))
+            connection.commit()
+            
+            cursor.execute(select_rating_query, (movie_id, reviewer_id))
+            result = cursor.fetchall()
+            
             for row in result:
                 print(row)
 except Error as e:
